f167d2e3cdc4a8c31ed8fb162dfd0cf80f80a755
Korux <dongke@oregonstate.edu>
2022-03-18 19:12:13 -0700

    up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 
